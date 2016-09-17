c019704de424b88b07e64762bf50de1c7059a7dd
engle2192 <go2engle@gmail.com>
2016-09-16 21:15:51 -0400

    Created installer

---
+++ PoGoMap-GUI_installer/PoGoMap-GUI_installer/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace PoGoMap_GUI_installer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 62);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(404, 23);
+            this.progressBar.TabIndex = 0;
+            this.progressBar.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(428, 97);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Installer";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 97);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PoGoMap-GUI Installer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PoGoMap-GUI_installer/PoGoMap-GUI_installer/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Diagnostics;
+using System.IO;
+
+namespace PoGoMap_GUI_installer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string DPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            if (Directory.Exists(@"C:\Program Files\Git"))
+            {
+                label1.Text = "Installing PoGoMap-GUI";
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = @"/C git clone https://github.com/engle2192/PoGoMap-GUI.git";
+                process.StartInfo = startInfo;
+                process.Start();
+                process.WaitForExit();
+                label1.Text = "Please close window to launch PoGoMap-GUI";
+
+            }
+            else
+            {
+                progressBar.Visible = true;
+                label1.Text = "Downloading Git";
+                WebClient client = new WebClient();
+                client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+                client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+                // Starts the download
+                client.DownloadFileAsync(new Uri("http://github.com/git-for-windows/git/releases/download/v2.10.0.windows.1/Git-2.10.0-64-bit.exe"), DPath + @"\github.exe");
+            }
+        }
+
+
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            double bytesIn = double.Parse(e.BytesReceived.ToString());
+            double totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+            double percentage = bytesIn / totalBytes * 100;
+            progressBar.Value = int.Parse(Math.Truncate(percentage).ToString());
+        }
+        void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            progressBar.Visible = false;
+            //MessageBox.Show("Download Completed");
+            label1.Text = "Installing Git";
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "github.exe";
+            startInfo.Arguments = "/verysilent";
+            process.StartInfo = startInfo;
+            process.Start();
+            process.WaitForExit();
+
+            label1.Text = "Waiting a Sec";
+            System.Diagnostics.Process process2 = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo2 = new System.Diagnostics.ProcessStartInfo();
+            startInfo2.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo2.FileName = "cmd.exe";
+            startInfo2.Arguments = "/C ping 127.0.0.1";
+            process2.StartInfo = startInfo2;
+            process2.Start();
+            process2.WaitForExit();
+
+
+            label1.Text = "Installing PoGoMap-GUI";
+            System.Diagnostics.Process process1 = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo1 = new System.Diagnostics.ProcessStartInfo();
+            startInfo1.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo1.FileName = "cmd.exe";
+            startInfo1.Arguments = @"/C git clone https://github.com/engle2192/PoGoMap-GUI.git";
+            process1.StartInfo = startInfo1;
+            process1.Start();
+            process1.WaitForExit();
+            label1.Text = "Please close window to launch PoGoMap-GUI";
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            System.Diagnostics.Process.Start(@".\PoGoMap-GUI\PokemonGo-Map_Launcher");
+        }
+    }
+}
